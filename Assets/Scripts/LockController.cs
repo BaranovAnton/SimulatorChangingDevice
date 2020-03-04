175aac00932d3ec2b21127f7fd0ffeb59b45915a
BaranovAnton <baranov_anton@inbox.ru>
2020-03-04 22:57:18 +0300

    Initial project

---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
